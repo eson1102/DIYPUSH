b17c4f304f2eacab501e44d4320e1871f4742519
eson1102 <duncanyu1102@gmail.com>
2024-04-09 17:47:04 +0800

    Update vps2.py

--- vps2.py
+++ vps2.py
@@ -27,7 +27,8 @@ accounts = {
     'account13': {'name': 'bjtuunopmv@iubridge.com', 'cookie': 'PPgySUq%252bC8tOjdWsbRettQ%253d%253d%257ctom8726%257chttps%253a%252f%252fimg.fx696.com%252fWikiEnterprise%252fsign%252fpersonph.png_wiki-template-global%257c0758300249%257cfff4524f009cb3b18111febec07c3a08'},
     # 添加更多账号信息
     'account14': {'name': 'gmqnfhbfks@iubridge.com', 'cookie': 'NyQQUJInoD79V7FBklGemw%253d%253d%257chhh457%257chttps%253a%252f%252fimg.fx696.com%252fWikiEnterprise%252fsign%252fpersonph.png_wiki-template-global%257c4433946647%257c7ced40c464cda3e923cc74c0ea1547fd'},
-    # 添加更多账号信息
+    'account15': {'name': 'tmifmahoge@iubridge.com', 'cookie': 'TPrf8cfx2MgVm3OJR9nQ0A%253d%253d%257chhhh723%257chttps%253a%252f%252fimg.fx696.com%252fWikiEnterprise%252fsign%252fpersonph.png_wiki-template-global%257c3213176112%257c538a37f6a60199da2c299ee8c8cff6a5'},
+    'account16': {'name': 'gkvcciymld@iubridge.com', 'cookie': 'dIaXUzSBBYnFfAfiS3kNsA%253d%253d%257cggg5352%257chttps%253a%252f%252fimg.fx696.com%252fWikiEnterprise%252fsign%252fpersonph.png_wiki-template-global%257c4433028554%257c845f9e87bd9fc26ca63e0e960b2f0c7a'},
 }
 
 url = 'https://vps.wikifx.com/zh-cn/jyzh'
