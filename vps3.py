e59a5576fa3794bd7a680e234404eb91e3263f1b
eson1102 <duncanyu1102@gmail.com>
2024-08-02 13:28:21 +0800

    Update vps3.py

--- vps3.py
+++ vps3.py
@@ -16,6 +16,10 @@ cookies_list = [
         'DJkdikKMG': '6OqTtw%252bzch7fL2BJvNgHLQ%253d%253d%257cFX3565537695%257chttps%253a%252f%252fimg.fx696.com%252fWikiEnterprise%252fsign%252fpersonph.png_wiki-template-global%257c5860011989%257c1716000c3d7ab46bf88309f2e7d77bc0',
         'remark': 'vgfvxixz@idrrate.com'
     },
+    {
+        'DJkdikKMG': 'codrpWmCzHjBTYo%252bG79kPQ%253d%253d%257cJim6153%257chttps%253a%252f%252fimg.fx696.com%252fthirdparty%252f4769447162%252f4769447162_28637.png_wiki-template-global%257c4423811951%257ca90a7db32d8c6f1833b9e43cac38c93f',
+        'remark': 'duncanyu1102@gmail.com'
+    },
     # 添加更多账号的cookies和备注
 ]
 
