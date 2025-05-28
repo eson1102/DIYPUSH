844a15cb9131173001bd90d408bb7ecdfde6980a
eson1102 <duncanyu1102@gmail.com>
2025-05-28 12:39:13 +0800

    Update clashupdate.py

--- clashupdate.py
+++ clashupdate.py
@@ -11,7 +11,7 @@ telegram_token = os.environ["BOT_TOKEN"]
 telegram_chat_id = os.environ["CHAT_ID"]
 
 # 使用请求库获取ini文件的内容
-response = requests.get("https://subapi.cmliussss.net/sub?&url=https%3A%2F%2Fcfdingyue-4lu.pages.dev%2Fa5ce27a6-76df-48da-be62-493fbda9a3af&target=clash&emoji=true&append_type=true&append_info=true&scv=true&udp=false&list=false&sort=false&fdn=false&insert=false",timeout=200)
+response = requests.get("https://subapi.cmliussss.net/sub?&url=https%3A%2F%2Fcfdingyue-4lu.pages.dev%2Fa5ce27a6-76df-48da-be62-493fbda9a3af&target=clash&emoji=true&append_type=true&append_info=false&scv=false&udp=false&list=false&sort=false&fdn=false&insert=false",timeout=200)
 file_content = response.content
 print(response)
 # 腾讯云COS的配置和客户端初始化
